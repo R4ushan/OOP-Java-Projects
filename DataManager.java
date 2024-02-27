e470902af19d3f7de2728e549445b5ef815e7c48
Silenzd <148633071+Silenzd@users.noreply.github.com>
2024-02-27 18:10:02 -0500

    Add files via upload

---
+++ DataManager.java
@@ -0,0 +1,16 @@
+public class DataManager {
+
+    DataManager() {}
+
+    public String getHello() {
+        return "Hello World";
+    }
+
+    public String getHowdy() {
+        return "Howdy y'all";
+    }
+
+    public String getChinese() {
+        return "Ni hao";
+    }
+}
